d1bebbe7ad7ff69e00c795f463442c21f1882025
Gabriel <gabrielletris29@gmail.com>
2026-06-22 10:54:49 +0700

    Add compiled Executable (Installer) and Batch script

---
+++ Jalankan_Compiler.bat
@@ -0,0 +1,11 @@
+@echo off
+title Pontianak Compiler - Eksekusi
+echo ========================================================
+echo   MENJALANKAN PONTIANAK COMPILER
+echo   Karya: Gabriel Bernard Rafa Radjali (06TPLP014)
+echo ========================================================
+echo.
+java -jar PontianakCompiler.jar
+echo.
+echo ========================================================
+pause
